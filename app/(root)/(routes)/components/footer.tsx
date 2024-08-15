af665dc7816ad7dcbcf9c2eb4ae9e82785afd42d
lucas <lucas.chorolqui@gmail.com>
2024-08-15 18:41:41 -0300

    footer styles

--- app/(root)/(routes)/components/footer.tsx
+++ app/(root)/(routes)/components/footer.tsx
@@ -1,32 +1,60 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-slate-50 text-slate-200 pt-8 pb-2">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div>
-            <h3 className="font-bold text-lg mb-3">Llamanos</h3>
-            <p>11 4798-6292</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 text-black">
+          <div className="flex">
+            <span>LOGO</span>
+            <div>
+              <h3 className="font-bold text-lg mb-1 tracking-[.3rem]">
+                Llamanos
+              </h3>
+              <p className="text-sm font-light">388-352342</p>
+            </div>
           </div>
-          <div>
-            <h3 className="font-bold text-lg mb-3">Email</h3>
-            <p>panaderia@quibeluma.com.ar</p>
+
+          <div className="flex">
+            <span>LOGO</span>
+            <div>
+              <h3 className="font-bold text-lg mb-1 tracking-[.3rem]">Email</h3>
+              <p className="text-sm font-light">elArtesano@gmail.com</p>
+            </div>
           </div>
-          <div>
-            <h3 className="font-bold text-lg mb-3">Visitanos</h3>
-            <p>Av. Fleming 1412, Martínez.</p>
+
+          <div className="flex">
+            <span>LOGO</span>
+            <div>
+              <h3 className="font-bold text-lg mb-1 tracking-[.3rem]">
+                Visitanos
+              </h3>
+              <p className="text-sm font-light">
+                Esquina Oscar Orinas y 9 de Julio.
+              </p>
+            </div>
           </div>
-          <div>
-            <h3 className="font-bold text-lg mb-3">Abrimos</h3>
-            <p>Lunes a Sábado: 7 a 20 hs.</p>
-            <p>Domingos: 7 a 19.30 hs.</p>
+
+          <div className="flex">
+            <span>LOGO</span>
+            <div>
+              <h3 className="font-bold text-lg mb-1 tracking-[.3rem]">
+                Abrimos
+              </h3>
+              <p className="text-sm font-light">Lunes a Sábado: 06 a 22 hs.</p>
+              <p className="text-sm font-light">Domingos: cerrado</p>
+            </div>
           </div>
         </div>
-        <div className="mt-8 text-center text-sm">
-          <p>
-            &copy; {new Date().getFullYear()} Panadería Quibeluma. Todos los
-            derechos reservados.
-          </p>
-        </div>
+      </div>
+
+      <picture className="flex w-full h-full justify-center items-end mt-8 mb-6">
+        <img src="./logo.png" alt="logo" className="rounded-full w-36" />
+      </picture>
+
+      <div className="mt-2 text-center text-sm text-black font-light">
+        <p>
+          &copy; {new Date().getFullYear()} Panadería El Artesano. Todos los
+          derechos reservados.
+        </p>
       </div>
     </footer>
   );
